29c1037d9c0f01a183b56e181cde3362d3ea8cef
HumbertoArellano <humbertoyham25@gmail.com>
2024-11-25 22:31:25 -0700

    Started API REST

---
+++ src/main/java/com/webapplication/latidopartners/LatidopartnersApplication.java
@@ -0,0 +1,13 @@
+package com.webapplication.latidopartners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatidopartnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatidopartnersApplication.class, args);
+	}
+
+}
